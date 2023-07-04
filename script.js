202392304ed53b2982e3ea37b844eddedbfb9bb6
MuhDavin213 <davin.new58@gmail.com>
2023-07-04 21:29:05 +0700

    register dgn fungsi penyimpanan di local storage

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.querySelector("#login-form");
-
-    loginForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Menghentikan submit form agar halaman tidak ter-refresh
-
-        const email = document.querySelector("#email").value;
-        const password = document.querySelector("#password").value;
-
-        // Lakukan pengecekan email dan password yang sesuai
-        if (email === "a@gmail.com" && password === "a") {
-            // Redirect ke halaman setelah berhasil login
-            window.location.href = "index.html";
-        } else {
-            // Tampilkan pesan kesalahan
-            alert("Email atau password salah. Silakan coba lagi.");
-        }
-    });
-});
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
@@ -36,4 +15,7 @@ document.getElementById("logout-btn").addEventListener("click", function() {
     // Alihkan ke halaman login atau halaman lainnya
     window.location.href = "login.html";
   });
+
   
+ // register
+ 
