dac24eed03c92b7bd9e9b61d99a7b752b7e83b9f
MuhDavin213 <davin.new58@gmail.com>
2023-07-05 00:11:52 +0700

    menu-toggle

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const sidebarNav = document.querySelector(".sidebar-nav");
-
+  
     menuToggle.addEventListener("click", function () {
-        menuToggle.classList.toggle("active");
-        sidebarNav.classList.toggle("active");
+      menuToggle.classList.toggle("active");
+      sidebarNav.classList.toggle("active");
     });
-});
+  });
+  
 
 document.getElementById("logout-btn").addEventListener("click", function() {
     // Hapus sesi pengguna atau lakukan tindakan logout lainnya
